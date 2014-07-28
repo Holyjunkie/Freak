7c8e961e7c6592ba3d692770e39f287cb9b54f59
Karl Garneau <karlgarneau@gmail.com>
2014-07-27 21:00:38 -0600

    Contains individual tree prefabs and an attempt at random generation.

---
+++ Assets/RandomTreeSpawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomTreeSpawn : MonoBehaviour {
+	
+	public GameObject MapleTree;
+	public float rateOfSpawn = 1f;
+
+	float treeMax = 5000;
+	private float nextSpawn = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (treeMax > nextSpawn) {
+			nextSpawn += rateOfSpawn;
+
+			Vector3 rndPosWithin;
+			rndPosWithin = new Vector3 (Random.Range(-2000f, 2000f), Random.Range(-2000f, 2000f), 0);
+			rndPosWithin = transform.TransformPoint(rndPosWithin * .5f);
+			Instantiate (MapleTree, rndPosWithin, transform.rotation);
+
+			Debug.Log("Spawned a tree.");
+
+			if(Physics.CheckSphere(transform.position, 100)){
+				Debug.Log ("lel");
+			}
+
+		}
+	}
+}
